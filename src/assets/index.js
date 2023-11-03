2db66bc44830c0f3b9d9a95d4ecb131bd659dcfa
Ines Mokaddem <ines.mokaddem@gmail.com>
2023-11-03 11:58:52 +0100

    🔥 F05: remove images and code not used for this project

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,20 +13,6 @@ import sport from "./sport.png"
 import massage from "./massage.png"
 
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
 import icon_expertise from "./icon_expertise.png"
 
 import carrent from "./carrent.png";
@@ -42,19 +28,6 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
   carrent,
   jobit,
   tripguide,
